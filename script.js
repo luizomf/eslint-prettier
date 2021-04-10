512a88d6c6859fc62e59b4cdb1b21c5c76b47178
Luiz Otavio <luizomf@gmail.com>
2021-04-10 13:03:27 -0300

    Add rule no-unused-vars

--- script.js
+++ script.js
@@ -15,11 +15,11 @@ const bgGreen = (...args) => `\u001b[30m\u001b[42m${args.join(' ')}${endColor}`;
 const white = (...args) => `\u001b[37m${args.join(' ')}${endColor}`;
 const cyan = (...args) => `\u001b[36m${args.join(' ')}${endColor}`;
 const magenta = (...args) => `\u001b[35m${args.join(' ')}${endColor}`;
-const blue = (...args) => `\u001b[34m${args.join(' ')}${endColor}`;
 const yellow = (...args) => `\u001b[33m${args.join(' ')}${endColor}`;
 const green = (...args) => `\u001b[32m${args.join(' ')}${endColor}`;
 const red = (...args) => `\u001b[31m${args.join(' ')}${endColor}`;
-const black = (...args) => `\u001b[30m${args.join(' ')}${endColor}`;
+const _blue = (...args) => `\u001b[34m${args.join(' ')}${endColor}`;
+const _black = (...args) => `\u001b[30m${args.join(' ')}${endColor}`;
 
 const isYes = (answer) => answer.toLocaleLowerCase().startsWith('s');
 const isNo = (answer) => !isYes(answer);
